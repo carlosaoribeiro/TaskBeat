d43ce52078fdf3fa022e5941158ee8ee4809e836
carlosribeiro <carlos.a.o.r@icloud.com>
2025-01-19 17:58:26 -0600

    Update arquivos para finalizar o crud

--- app/src/main/java/com/devspace/taskbeats/CategoryDao.kt
+++ app/src/main/java/com/devspace/taskbeats/CategoryDao.kt
@@ -1,15 +1,23 @@
 package com.devspace.taskbeats
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface CategoryDao {
-        @Insert(onConflict = OnConflictStrategy.REPLACE)
-        suspend fun insertAll(categories: List<CategoryEntity>)
 
-        @Query("SELECT * FROM CategoryEntity")
-        suspend fun getAll(): List<CategoryEntity>
+        @Query("Select * From categoryentity")
+        fun getAll(): List<CategoryEntity>
+
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        fun insetAll(categoryEntity: List<CategoryEntity>)
+
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        fun inset(categoryEntity: CategoryEntity)
+
+        @Delete
+        fun delete(categoryEntity: CategoryEntity)
 }
